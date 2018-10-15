43bf6a5ca876fd2fbedf8ccc85811400cfcf5870
Zakir Hossen <web.zakirbd@gmail.com>
2018-10-15 21:49:28 +0600

    First commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-sm m-2 btn-info"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-sm m-2 btn-danger"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+}
+
+export default Counter;
